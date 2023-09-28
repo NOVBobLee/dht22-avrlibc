d5a47642da2c03faf721a796f5d8231f1cc9384d
NOVBobLee <defru04002@gmail.com>
2023-09-29 00:57:00 +0800

    Modify the DHT22 code to be more accurate and concise

--- dht22.c
+++ dht22.c
@@ -1,46 +1,112 @@
-#include <avr/interrupt.h>
 #include <util/delay.h>
 #include "dht22.h"
 
-#define MAX_LOOP 100
+#define TIMEOUT_LOOPS 50
 
-#define DECLARE_WAIT_FOR_CHANGE(number, min_us, max_loop)			\
-static inline int wait_for_change ## number(struct dht22_ctx *dev,	\
-											uint8_t expected)		\
-{																	\
-	*dev->ddr &= ~_BV(dev->bit);									\
-	_delay_us(min_us);												\
-	for (int i = 0; i < max_loop; ++i) {							\
-		if (expected == (*dev->pin & _BV(dev->bit)))				\
-			return 0;	/* get expected */							\
-	}																\
-	return -1;			/* timeout */								\
+/* use loop for relaxing the delay us,ms */
+static inline int wait_for_change(struct dht22_ctx *dev, int reps,
+								  uint8_t expected)
+{
+	for (int i = 0; i < reps; ++i)
+		if (expected == (*dev->pin & _BV(dev->bit)))
+			return 0;	/* get expected */
+	return -1;			/* timeout */
 }
 
 static int check_pullup(struct dht22_ctx *dev)
 {
-	int ret = -1;
-
 	*dev->ddr &= ~_BV(dev->bit);
-	if (1 == (*dev->pin & _BV(dev->bit)))
-		ret = 0;
 
-	return ret;
+	if (1 == (*dev->pin & _BV(dev->bit)))
+		return 0;
+	return -1;
 }
 
-static int send_request(struct dht22_ctx *dev)
+static inline void send_request(struct dht22_ctx *dev)
 {
-	if (check_pullup(dev) < 0)
-		return -1;
-	_delay_ms(50);
-
-	/* set low(1ms) */
 	*dev->ddr |= _BV(dev->bit);
+
+	/* set low(>1ms) */
 	*dev->port &= ~_BV(dev->bit);
 	_delay_ms(1.1);
 
-	/* set high(20us) */
+	/* set high(20-40us) */
 	*dev->port |= _BV(dev->bit);
+	_delay_us(20);
+}
+
+static inline int confirm_ack(struct dht22_ctx *dev)
+{
+	*dev->ddr &= ~_BV(dev->bit);
+
+	/* confirm low(80us) */
+	if (wait_for_change(dev, TIMEOUT_LOOPS, 0) < 0)
+		return -1;
+
+	_delay_us(75);
+
+	/* confirm: high(80us) */
+	if (wait_for_change(dev, TIMEOUT_LOOPS, 1) < 0)
+		return -1;
+
+	_delay_us(75);
+	return 0;
+}
+
+static inline void parse_data(struct dht22_ctx *dev, uint8_t *data)
+{
+	dev->rh = .1f * (data[0] << 8 | data[1]);
+	dev->temp = .1f * ((data[2] & 0x7f) << 8 | data[3]);
+	if (data[2] & 0x80)
+		dev->temp = -dev->temp;
+}
+
+static inline int receive_data(struct dht22_ctx *dev)
+{
+	uint8_t data[5] = { 0 };
+
+	*dev->ddr &= ~_BV(dev->bit);
+
+	/* receive data, from MSB to LSB */
+	for (int i = 0; i < 5; ++i) {
+		for (int b = 7; b >= 0; --b) {
+			/* confirm low(50us) */
+			if (wait_for_change(dev, TIMEOUT_LOOPS, 0) < 0)
+				return -3;
+			_delay_us(50);
+
+			/* currently should be high */
+			if (0 == (*dev->pin & _BV(dev->bit)))
+				return -4;
+			_delay_us(20);
+
+			/* get bit 0 if confirm high(26-28us)
+			 *     bit 1 if confirm high(70us)
+			 * 15 loops is about 16 us */
+			if (wait_for_change(dev, 15, 0) < 0) {
+				data[i] |= _BV(b);
+
+				/* delay for remainder 30+ us */
+				_delay_us(30);
+				if (wait_for_change(dev, TIMEOUT_LOOPS, 0) < 0)
+					return -5;
+			}
+		}
+	}
+
+#ifdef _DHT_DEBUG
+	/* record the raw data */
+	for (int i = 0; i < 5; ++i)
+		dev->raw[i] = data[i];
+#endif
+
+	/* cksum */
+	if ((data[0] + data[1] + data[2] + data[3]) != data[4])
+		return -6;
+
+	/* translate the data */
+	parse_data(dev, data);
+
 	return 0;
 }
 
@@ -66,62 +132,20 @@ int dht22_init(struct dht22_ctx *dev, volatile uint8_t *ddr,
 	return check_pullup(dev);
 }
 
-DECLARE_WAIT_FOR_CHANGE(0, 20, MAX_LOOP);
-DECLARE_WAIT_FOR_CHANGE(1, 70, MAX_LOOP);
-DECLARE_WAIT_FOR_CHANGE(2, 40, MAX_LOOP);
-DECLARE_WAIT_FOR_CHANGE(3, 30, 1);
-DECLARE_WAIT_FOR_CHANGE(4, 10, MAX_LOOP);
 int dht22_getdata(struct dht22_ctx *dev)
 {
-	int ret = 0;
-	uint8_t data[5] = { 0 };
+	/* check initial pin state */
+	if (check_pullup(dev) < 0)
+		return -1;
+	_delay_ms(50);
 
-	/* send request: low(1ms) -> high(20-40us) */
-	if ((ret = send_request(dev)) < 0)
-		return ret;
+	/* send request to DHT22 */
+	send_request(dev);
 
-	/* check dht22 ACK: low(80us) */
-	if (wait_for_change0(dev, 0) < 0)
+	/* confirm ack responsed from DHT22 */
+	if (confirm_ack(dev) < 0)
 		return -2;
 
-	/* check dht22 ACK: high(80us) */
-	if (wait_for_change1(dev, 1) < 0)
-		return -3;
-
-	if (wait_for_change1(dev, 0) < 0)
-		return -4;
-
-	/* receive data, from MSB to LSB */
-	for (int i = 0; i < 5; ++i) {
-		for (int b = 7; b >= 0; --b) {
-			/* low(50us) */
-			if (wait_for_change2(dev, 1) < 0)
-				return -5;
-
-			/* 0 if high(26-28us)
-			 * 1 if high(70us) */
-			if (wait_for_change3(dev, 0) < 0) {
-				data[i] |= _BV(b);
-				if (wait_for_change4(dev, 0) < 0)
-					return -6;
-			}
-		}
-	}
-
-#ifdef _DHT_DEBUG
-	for (int i = 0; i < 5; ++i)
-		dev->raw[i] = data[i];
-#endif
-
-	/* cksum */
-	if ((data[0] + data[1] + data[2] + data[3]) != data[4])
-		return -7;
-
-	/* translate the data */
-	dev->rh = .1f * (data[0] << 8 | data[1]);
-	dev->temp = .1f * ((data[2] & 0x7f) << 8 | data[3]);
-	if (data[2] & 0x80)
-		dev->temp = -dev->temp;
-
-	return 0;
+	/* receive DHT22 data */
+	return receive_data(dev);
 }
