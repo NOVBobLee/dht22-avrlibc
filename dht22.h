462173a7d0758cfd6ef26575e22afa51d272f70e
NOVBobLee <defru04002@gmail.com>
2023-10-06 00:38:36 +0800

    Add API descriptions

--- dht22.h
+++ dht22.h
@@ -16,8 +16,30 @@ struct dht22_ctx {
 #endif
 };
 
+/**
+ * Initialize DHT22 device.
+ *
+ * @dev: struct dht22_ctx to hold the DHT22 device's settings and data
+ * @ddr: specify DDR the DHT22 uses
+ * @pin: specify PIN the DHT22 uses
+ * @port: specify PORT the DHT22 uses
+ * @bit: specify the bit on PIN and PORT which the DHT22 uses
+ *
+ * Return on 0: Success, -1: Failure
+ */
 int dht22_init(struct dht22_ctx *dev, volatile uint8_t *ddr,
 			   volatile uint8_t *pin, volatile uint8_t *port, uint8_t bit);
+
+/**
+ * Send request to DHT22 and get the data from it.
+ *
+ * @dev: specified which DHT22 device
+ *
+ * Return on 0: Success, <0: Failure
+ *
+ * Once success, the temperature and relative humidity will be written into
+ * dev->temp and dev->rh.
+ */
 int dht22_getdata(struct dht22_ctx *dev);
 
 #endif /* _DHT22_H */
